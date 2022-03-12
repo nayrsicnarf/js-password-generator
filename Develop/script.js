28a5c7bca0475d1fc2fad22835f2451b529985db
Francis Aguinaldo <fryaguinaldo@gmail.com>
2022-03-12 14:42:17 -0800

    Add updates

--- Develop/script.js
+++ Develop/script.js
@@ -45,5 +45,49 @@ function generatePassword() {
   var passwordLength = "";          // Sets the length of the password
   var possChar = "";                // Sets the possible characters based off the password length
   var createdPassword = "";         // Sets the randomly generated password
-  
+
+    // Set all booleans to false
+    var lowerBoolean = upperBoolean = numericBoolean = specialBoolean = false;
+
+    // if statement to check and make sure at least one value is selected
+    if (lowerBoolean === false && upperBoolean === false && numericBoolean === false && specialBoolean === false) {
+      var lowerBoolean = confirm("Do you want your password to contain a lowercase character? Press 'OK' for yes or 'Cancel' for no");
+      var upperBoolean = confirm("Do you want your password to contain a uppercase character? Press 'OK' for yes or 'Cancel' for no");
+      var numericBoolean = confirm("Do you want your password to contain a numerical character? Press 'OK' for yes or 'Cancel' for no");
+      var specialBoolean = confirm("Do you want your password to contain a special character? Press 'OK' for yes or 'Cancel' for no");
+    }
+
+    if (lowerBoolean && upperBoolean && numericBoolean && specialBoolean) {
+      return createPass(lowerString, upperString, numericString, specialString);
+      } else if (lowerBoolean && upperBoolean && numericBoolean) {
+      return createPass(lowerString, upperString, numericString);
+      } else if (lowerBoolean && upperBoolean && specialBoolean) {
+      return createPass(lowerString, upperString, specialString);
+      } else if (lowerBoolean && numericBoolean && specialBoolean) {
+      return createPass(lowerString, numericString, specialString);
+      } else if (lowerBoolean && upperBoolean) {
+      return createPass(lowerString, upperString);
+      } else if (lowerBoolean && numericBoolean) {
+      return createPass(lowerString, numericString);
+      } else if (lowerBoolean && specialBoolean) {
+      return createPass(lowerString, specialString);
+      } else if (upperBoolean && numericBoolean && specialBoolean) {
+      return createPass(upperString, numericString, specialString);
+      } else if (upperBoolean && numericBoolean) {
+      return createPass(upperString, numericString);
+      } else if (upperBoolean && specialBoolean) {
+      return createPass(upperString, specialString);
+      } else if (upperBoolean) {
+      return createPass(upperString);
+      } else if (numericBoolean && specialBoolean) {
+      return createPass(numericString, specialString);
+      } else if (numericBoolean) {
+      return createPass(numericString);
+      } else if (specialBoolean) {
+      return createPass(specialString);
+      } else if (lowerBoolean) {
+      return createPass(lowerString);
+      }
+
+      return createdPassword;
 }
